0645e9ece6e54a5eb481c042f4ba197eaed0b230
Eugene Butusov <EvgenyButusov@gmail.com>
2016-04-27 23:43:20 +0300

    initial commit

---
+++ restifizer/restifizer.m
@@ -0,0 +1,13 @@
+//
+//  restifizer.m
+//  restifizer
+//
+//  Created by Eugene Butusov on 27/04/16.
+//  Copyright © 2016 VedideV. All rights reserved.
+//
+
+#import "restifizer.h"
+
+@implementation restifizer
+
+@end
